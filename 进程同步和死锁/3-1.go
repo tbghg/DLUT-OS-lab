e99ac60c590ee08a116fc1ae722550b56b411b08
冰航 <tianbinghang@bilibili.com>
2023-06-05 11:35:56 +0800

    fix 生产者-消费者算法 添加自定义输入

--- 进程同步和死锁/3-1.go
+++ 进程同步和死锁/3-1.go
@@ -18,20 +18,15 @@ import (
 	通过使用信号量机制，程序保证了仓库中同时最多只能有10个物品，并且在仓库已满或已空时，生产者和消费者会被阻塞等待。
 */
 
-const (
-	maxItems = 10 // 仓库最大容量
-	numItems = 20 // 每个生产者和消费者需要生产/消费的物品总数
-)
-
 var (
-	wg    sync.WaitGroup             // 控制程序结束
-	mutex sync.Mutex                 // 互斥锁，保证同时只有一个线程对仓库进行操作
-	full  = make(chan int, maxItems) // 信号量，表示仓库中已有的物品数量
-	empty = make(chan int, maxItems) // 信号量，表示仓库中空余的物品数量
-	q     = make([]int, 0)           // 存放物品的队列
+	wg    sync.WaitGroup   // 控制程序结束
+	mutex sync.Mutex       // 互斥锁，保证同时只有一个线程对仓库进行操作
+	full  chan int         // 信号量，表示仓库中已有的物品数量
+	empty chan int         // 信号量，表示仓库中空余的物品数量
+	q     = make([]int, 0) // 存放物品的队列
 )
 
-func producer(id int) {
+func producer(id int, numItems int) {
 	defer wg.Done()
 
 	for i := 0; i < numItems; i++ {
@@ -46,7 +41,7 @@ func producer(id int) {
 	}
 }
 
-func consumer(id int) {
+func consumer(id int, numItems int) {
 	defer wg.Done()
 
 	for i := 0; i < numItems; i++ {
@@ -62,19 +57,35 @@ func consumer(id int) {
 }
 
 func main() {
-	// 初始化信号量，表示仓库中有空余位置
+	var maxItems, numItems, numProducers, numConsumers int
+
+	fmt.Print("请输入仓库最大容量(例如 10)：")
+	fmt.Scanln(&maxItems)
+
+	fmt.Print("请输入每个生产者和消费者需要生产/消费的物品数目(例如 20)：")
+	fmt.Scanln(&numItems)
+
+	fmt.Print("请输入生产者线程数(例如 5)：")
+	fmt.Scanln(&numProducers)
+
+	fmt.Print("请输入消费者线程数(例如 5)：")
+	fmt.Scanln(&numConsumers)
+
+	full = make(chan int, maxItems)  // 初始化信号量，表示仓库中已有的物品数量
+	empty = make(chan int, maxItems) // 初始化信号量，表示仓库中空余的物品数量
+
 	for i := 0; i < maxItems; i++ {
-		empty <- 1
+		empty <- 1 // 将空槽个数初始化为maxItems
 	}
-	// 创建生产者线程
-	for i := 0; i < 5; i++ {
+
+	for i := 0; i < numProducers; i++ {
 		wg.Add(1)
-		go producer(i)
+		go producer(i, numItems)
 	}
-	// 创建消费者线程
-	for i := 0; i < 5; i++ {
+
+	for i := 0; i < numConsumers; i++ {
 		wg.Add(1)
-		go consumer(i)
+		go consumer(i, numItems)
 	}
 
 	wg.Wait() // 等待所有线程结束
